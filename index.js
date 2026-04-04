2fbb2a439d2b070fc5fb74505cb778ff30c8ff72
enigmaDevelopments <enigmarezac@gmail.com>
2026-04-04 19:44:18 -0400

    made a simple sign in page

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 import { supabase }   from "./supabase-client.js";
+console.log("Hello world!");
+supabase
+  .from('test')
+  .insert({ test:  Math.floor(Math.random() * 10)}) );
 
+console.log("Inserted a random number into the test table!");
